b51b6df46cca30d0148e5f2d5b8df938157920a2
Jack Laird <jack.laird0@gmail.com>
2018-05-07 19:35:53 -0600

    Create Category component along with test and css file

---
+++ src/Stateless Components/CategoryCard/CategoryCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const CategoryCard = () => {
+  
+  return (
+    <div className="category-card">
+      <h2>Title</h2>
+      <button className="favorite">Star</button>
+      <h2>homewolrd</h2>
+      <h2>species</h2>
+      <h2>language</h2>
+      <h2>population</h2>
+    </div>
+  )
+}
+
+export default CategoryCard;
