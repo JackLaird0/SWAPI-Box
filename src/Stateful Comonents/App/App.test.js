82c20a2b546e3ddde856d2feb86ba521d65e976d
Jack Laird <jack.laird0@gmail.com>
2018-05-14 14:27:59 -0600

    Set up tests for App and test snapshot and favorites function

--- src/Stateful Comonents/App/App.test.js
+++ src/Stateful Comonents/App/App.test.js
@@ -1,15 +1,43 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { shallow, mount } from 'enzyme';
+import '../../setupTests';
 
-// describe('App tests', () => {
-//   beforeEach(() => {
-//     app = shallow(<App />, {disableLifecycleMethods: true})
-//   })
+describe('App tests', () => {
+  it('Should match the snapshot', () => {
+    const appComponent = shallow(<App />, {disableLifecycleMethods: true})
 
-//   it('renders without crashing', () => {
-//     const div = document.createElement('div');
-//     ReactDOM.render(<App />, div);
-//     ReactDOM.unmountComponentAtNode(div);
-//   });
-// })
+    expect(appComponent).toMatchSnapshot()
+  })
+
+  it('Should render with the correct default state', () => {
+    const appComponent = shallow(<App />, {disableLifecycleMethods: true})
+
+    expect(appComponent.state('vehicles')).toEqual([])
+    expect(appComponent.state('openingCrawl')).toEqual([]);
+    expect(appComponent.state('planets')).toEqual([]);
+    expect(appComponent.state('people')).toEqual([]);
+    expect(appComponent.state('selectedCategory')).toEqual('');
+    expect(appComponent.state('favorites')).toEqual([])
+  })
+
+  it('Should to the favorites state when a favorite is saved', () => {
+    const appComponent = shallow(<App />, {disableLifecycleMethods: true})
+    const favoriteData = {hey: 'hello'}
+    appComponent.instance().saveFavorites(favoriteData)
+
+    expect(appComponent.state('favorites')).toEqual([favoriteData])
+  })
+
+  it('Should add to the existing state when a favorite is saved', () => {
+    const appComponent = shallow(<App />, {disableLifecycleMethods: true})
+    const favoriteData = {hey: 'hello'}
+    const mockData = {hey: 'hello'}
+    appComponent.instance().saveFavorites(favoriteData)
+    appComponent.instance().saveFavorites(mockData)
+    const expected = [{hey: 'hello'}, {hey: 'hello'}]
+
+    expect(appComponent.state('favorites')).toEqual(expected)
+  })
+})
